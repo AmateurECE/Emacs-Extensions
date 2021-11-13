da57b69aed12a6ca4150781b705dc56fda958580
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-11-12 22:22:24 -0600

    Add MIT license to insert-banner.el

--- insert-banner.el
+++ insert-banner.el
@@ -68,23 +68,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.")
 
 (defconst file-mit-license
   "\
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the \"Software\"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the \"Software\"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.")
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.")
 
 (defvar my-name (getenv "GIT_AUTHOR_NAME"))
 (defvar my-email (getenv "GIT_AUTHOR_EMAIL"))
@@ -204,6 +204,8 @@ SOFTWARE.")
   (cond
    ((eq file-copyright-license 'file-gplv3-license)
     file-gplv3-license)
+   ((eq file-copyright-license 'file-mit-license)
+    file-mit-license)
    (t nil)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
