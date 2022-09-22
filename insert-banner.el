9c92ec2cc6c096dabc8344110b0651d344eb0a84
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-09-22 11:21:18 -0500

    Add GPLv2 License

--- insert-banner.el
+++ insert-banner.el
@@ -66,6 +66,21 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.")
 
+(defconst file-gplv2-license
+  "\
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.")
+
 (defconst file-mit-license
   "\
 Permission is hereby granted, free of charge, to any person obtaining a copy
